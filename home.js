35bb9cc5d92b3a0fdc7a16234dced5c24223b452
Rômulo Gomes <romulo.gomes.mendes@hotmail.com>
2022-07-29 01:53:33 -0300

    scripts adjustments and create localstorage

---
+++ home.js
@@ -0,0 +1,2 @@
+var allData = JSON.parse(localStorage.getItem("allData"));
+console.log(allData);
--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-/*Login */
-
 var $loginForm = document.getElementById("login-form");
 $loginForm.addEventListener("submit", async (e) => {
 	e.preventDefault();
 	const response = await fetch("./data.json");
 	var allData = await response.json();
+	localStorage.setItem("allData", JSON.stringify(allData.data));
+	console.log(allData);
 	var users = allData.data.login;
 
 	let $loginUser = document.getElementById("loginUser");
 	let $loginPass = document.getElementById("loginPass");
 
-	if (!$loginUser.value || !$loginPass.value) {
+	if (!$loginUser.value || !$loginPass.value)
 		return alert("Preencha todos os campos antes de continuar");
-	} else {
-		users.filter((element) => {
-			if (
-				element.email == $loginUser.value &&
-				element.password == $loginPass.value
-			) {
-				window.location.assign("./home.html");
-				return;
-			}
-		});
-	}
-	alert("Usuário ou senha inválido");
+	const userChecked = users.some(
+		(element) =>
+			element.email == $loginUser.value && element.password == $loginPass.value
+	);
+	if (!userChecked) return alert("Usuário ou senha inválido");
+
+	window.location.assign("./home.html");
 });
